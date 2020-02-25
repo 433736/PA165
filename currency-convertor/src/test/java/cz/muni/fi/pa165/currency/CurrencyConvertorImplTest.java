f8f279be6c4f40394b027e0e7a301ce82b13d1cc
Jan Horvath <xhorvat6@nymfe64.fi.muni.cz>
2020-02-25 11:26:46 +0100

    WIP task 02

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,19 +1,55 @@
 package cz.muni.fi.pa165.currency;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
 
+    private static Currency eurCurrency = Currency.getInstance("EUR");
+    private static Currency czkCurrency = Currency.getInstance("CZK");
+
+    @Mock
+    private ExchangeRateTable exchangeRateTable;
+
+    private CurrencyConvertor currencyConvertor;
+
+    @Rule
+    ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void init() {
+        currencyConvertor = new CurrencyConvertorImpl(exchangeRateTable);
+    }
+
     @Test
-    public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+    public void testConvert() throws ExternalServiceFailureException {
+        when(exchangeRateTable.getExchangeRate(eurCurrency, czkCurrency))
+                .thenReturn(new BigDecimal("10.0"));
+
+        assertEquals(new BigDecimal(24.12), currencyConvertor.convert(eurCurrency, czkCurrency, new BigDecimal(2.4125)));
+        assertEquals(new BigDecimal(24.12), currencyConvertor.convert(eurCurrency, czkCurrency, new BigDecimal(2.4115)));
+        assertEquals(new BigDecimal(24.13), currencyConvertor.convert(eurCurrency, czkCurrency, new BigDecimal(2.4126)));
+        assertEquals(new BigDecimal(24.12), currencyConvertor.convert(eurCurrency, czkCurrency, new BigDecimal(2.4124)));
     }
 
     @Test
     public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+        expected
+
     }
 
     @Test
